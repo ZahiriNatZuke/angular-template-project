cdce7b1fcd172b449fc77dcdba0e2272228c69ba
Yohan González Almaguer <yohan.gonzalez.almaguer@gmail.com>
2024-06-19 20:01:46 -0400

    feat: improve theme service

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -1,5 +1,5 @@
-import { inject, Injectable, RendererFactory2, signal } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { computed, inject, Injectable, PLATFORM_ID, RendererFactory2, signal } from '@angular/core';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { Themes } from '@core/types/enums';
 import { environment } from '@core/environments';
@@ -9,38 +9,40 @@ export class ThemeService {
 
   #document: Document = inject(DOCUMENT);
   #htmlElement = this.#document.querySelector('html');
-  #bodyElement = this.#document.body;
   #renderer2 = inject(RendererFactory2).createRenderer(this.#document, null);
   #mediaMatcher = inject(MediaMatcher);
-  #theme = signal(Themes.Light);
+  #theme = signal<Themes>(Themes.Light);
+  #platform = inject(PLATFORM_ID);
 
-  get isDarkMode() {
-    return this.#theme() === Themes.Dark;
-  }
+  isDarkMode = computed(() => this.#theme() === Themes.Dark);
 
-  get currentTheme(): Themes {
-    return this.#theme();
+  get theme() {
+    return this.#theme.asReadonly();
   }
 
   init() {
-    if ( localStorage.getItem(environment.themeKey) ) {
-      this.#theme.set(localStorage.getItem(environment.themeKey) as Themes);
+    if ( isPlatformBrowser(this.#platform) ) {
+      if ( localStorage.getItem(environment.themeKey) ) {
+        this.#theme.set(localStorage.getItem(environment.themeKey) as Themes);
+      } else {
+        const themeQuery = this.#mediaMatcher.matchMedia('(prefers-color-scheme: dark)');
+        this.#theme.set(themeQuery.matches ? Themes.Dark : Themes.Light);
+        localStorage.setItem(environment.themeKey, this.#theme());
+      }
       this.#renderer2.setAttribute(this.#htmlElement, 'data-theme', this.#theme());
-    } else {
-      const themeQuery = this.#mediaMatcher.matchMedia('(prefers-color-scheme: dark)');
-      this.#theme.set(themeQuery.matches ? Themes.Dark : Themes.Light);
-      localStorage.setItem(environment.themeKey, this.#theme());
-      this.#renderer2.setAttribute(this.#htmlElement, 'data-theme', this.#theme());
+      this.#renderer2.removeClass(this.#document.body, ( this.#theme() === Themes.Dark ) ? Themes.Light : Themes.Dark);
+      this.#renderer2.addClass(this.#document.body, this.#theme());
     }
-    this.#renderer2.addClass(this.#bodyElement, this.currentTheme);
   }
 
   setTheme(theme: Themes) {
-    this.#renderer2.setAttribute(this.#htmlElement, 'data-theme', theme);
-    this.#renderer2.removeClass(this.#bodyElement, this.currentTheme);
-    this.#renderer2.addClass(this.#bodyElement, theme);
-    localStorage.setItem(environment.themeKey, theme);
-    this.#theme.set(theme);
+    if ( isPlatformBrowser(this.#platform) ) {
+      this.#renderer2.setAttribute(this.#htmlElement, 'data-theme', theme);
+      this.#renderer2.removeClass(this.#document.body, this.#theme());
+      this.#renderer2.addClass(this.#document.body, theme);
+      localStorage.setItem(environment.themeKey, theme);
+      this.#theme.set(theme);
+    }
   }
 
 }
