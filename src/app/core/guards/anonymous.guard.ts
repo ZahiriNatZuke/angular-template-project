bf77b57af627ef4ede299dbe750f6129f4cc8ac6
Yohan González Almaguer <yohan.gonzalez.almaguer@gmail.com>
2025-12-20 17:21:35 -0300

    refactor: simplify anonymous guard logic by removing unnecessary router injection

--- src/app/core/guards/anonymous.guard.ts
+++ src/app/core/guards/anonymous.guard.ts
@@ -1,15 +1,9 @@
 import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import { CanActivateFn } from '@angular/router';
 import { AuthStore } from '@core/stores';
 
 export const anonymousGuard: CanActivateFn = (_route, _state) => {
 	const authStore = inject(AuthStore);
-	const router = inject(Router);
 
-	if (authStore.isAnonymous()) {
-		return true;
-	}
-
-	router.navigate(['/dashboard']);
-	return false;
+	return authStore.isAnonymous();
 };
